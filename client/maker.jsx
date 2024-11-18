1aa7672fc63e426476898510a84f9a34b95d0f9a
CAZtheArcher <caz1158@g.rit.edu>
2024-11-17 21:58:48 -0500

    part 1 of DomoMaker E

--- client/maker.jsx
+++ client/maker.jsx
@@ -32,6 +32,8 @@ const DomoForm = (props) => {
             <input id="domoName" type="text" name="name" placeholder="Domo Name" />
             <label htmlFor="age">Age: </label>
             <input id="domoAge" type="number" min="0" name="age" />
+            <label htmlFor="power">Power Level: </label>
+            <input id="domoPower" type="number" min="0" name="power" />
             <input className="makeDomoSubmit" type="submit" value="Make Domo" />
         </form>
     );
@@ -63,6 +65,7 @@ const DomoList = (props) => {
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="domoName">Name: {domo.name}</h3>
                 <h3 className="domoAge">Age: {domo.age}</h3>
+                <h3 className="domoPower">Power Level: {domo.power}</h3>
             </div>
         );
     });
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -5,20 +5,21 @@ const Domo = models.Domo;
 const makerPage = async (req, res) => res.render('app');
 
 const makeDomo = async (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'Both name and age are required!' });
+  if (!req.body.name || !req.body.age || !req.body.power) {
+    return res.status(400).json({ error: 'Name, age, and power level are required!' });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
+    power: req.body.power,
     owner: req.session.account._id,
   };
 
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age, power: newDomo.power });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
